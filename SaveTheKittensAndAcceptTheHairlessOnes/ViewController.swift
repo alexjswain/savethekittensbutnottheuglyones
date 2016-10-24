a8b4026c90ff76db66ba2ed580a74b9bc7e08ad6
Show CPU <dan@dta-show-cpu.att.net>
2016-10-24 13:56:42 -0700

    Initial Commit

---
+++ SaveTheKittensAndAcceptTheHairlessOnes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheKittensAndAcceptTheHairlessOnes
+//
+//  Created by Show CPU on 10/24/16.
+//  Copyright © 2016 Show CPU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
